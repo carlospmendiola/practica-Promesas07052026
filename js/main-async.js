b505117264eef837da75490fca802be64f852b46
carlospmendiola <carlos@carlospmendiola.com>
2026-05-07 18:20:40 +0200

    Se han añadido los estilos css al archivo

---
+++ js/main-async.js
@@ -0,0 +1,33 @@
+// VARIABLES
+const btnMostrar = document.querySelector ('#mostrarUsuario')
+const pintaUsuario = document.querySelector ('#pintaUsuario')
+
+
+// FUNCIONES
+//para recibir los datos del usuario creamos la función de la llamada a la API desde el botón
+function llamarApi() {
+  return new Promise((resolve, reject) => {
+    let conexion = true
+    setTimeout(() => {
+    if (conexion) {
+      let alumno = {nombre: 'pepe', correo:'pepe@correo.es'}
+      resolve(alumno);
+    } else {
+      reject ('Error: conexion perdida, intenta mas tarde')
+    }
+    }, 2000);
+  });
+}
+
+btnMostrar.addEventListener('click', ()=>{
+llamarApi().then(resolve=>{
+// aqui muestra los datos usuario
+let mensaje = `El nombre del usuario es: ${resolve.nombre} y su correo: ${resolve.correo}`
+    pintaUsuario.textContent = mensaje
+    })
+          .catch(reject =>{
+    //aqui mostrar error al usuario
+    pintaUsuario.textContent = reject
+    })
+})
+
